d96086897abc96d48701fb031ab2af5b411e6288
gabuyama68@gmail.com <gabuyama68@gmail.com>
2022-03-20 15:39:34 +0530

    wip

--- src/pages/section4.js
+++ src/pages/section4.js
@@ -3,15 +3,24 @@ import React from 'react'
 import "./styles.css"
 
 const section4 = ({ user }) => {
+  console.log(user);
   return (
     <div className="section-4-container">
-        <h1>Location Details</h1>
-        <div className="section-4-detils">
-            <p>Address</p> <p>Kolkata</p>
-            <p>District</p> <p>South</p>
-            <p>Pincode</p> <p>700039</p>
-            <p>State</p> <p>WB</p>
-        </div>
+      <h1>Location Details</h1>
+      <div className="section-4-detils">
+        {user?.address && <>
+          <p>Address</p> <p>{user.address}</p>
+        </>}
+        {user?.district && <>
+          <p>District</p> <p>{user.district}</p>
+        </>}
+        {user?.pincode && <>
+          <p>Pincode</p> <p>{user.pincode}</p>
+        </>}
+        {user?.state && <>
+          <p>State</p> <p>{user.state}</p>
+        </>}
+      </div>
     </div>
   )
 }
